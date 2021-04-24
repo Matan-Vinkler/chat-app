502096eb1e6ecaf2a62a2636f24e6a00f7c6a04b
Matan Vinkler <matanvinkler@gmail.com>
2021-04-24 23:07:38 +0300

    LL

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,8 +1,21 @@
-const generateMessage = (username, text) => {
+const Room = require("../db/rooms")
+
+const generateMessage = (username, text, roomName) => {
+    var room = Room.findOne({Name: roomName})
+    var date = new Date().getTime()
+
+    room.Messages.push({
+        username,
+        date,
+        text
+    })
+
+    room.save()
+
     return {
         username,
         text,
-        createdAt: new Date().getTime()
+        createdAt: date
     }
 }
 
