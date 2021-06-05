b15b444bc0bbdbace1f55298af931f04433fe8ff
Matan Vinkler <matanvinkler@gmail.com>
2021-06-06 00:03:25 +0300

    8

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/chat-app-saver", {
+mongoose.connect(process.env.MONGODB_PATH, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
